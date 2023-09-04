9a66abd81c7a49c79162f3f020a70af60a0555d7
Gleyson Atanázio <gleysonasilva@gmail.com>
2023-09-04 20:05:36 -0300

    cria codigo inicial

--- bin/calcutator.dart
+++ bin/calcutator.dart
@@ -5,5 +5,14 @@ import 'dart:io';
 void main(List<String> arguments) {
   print('Bem vindo!');
   print('Informe o primeiro número: ');
-  
+  var line = stdin.readLineSync(encoding: utf8);
+  var n1 = double.parse(line ?? "0");
+
+  print('Informe o segundo número: ');
+  line = stdin.readLineSync(encoding: utf8);
+  var n2 = double.parse(line ?? "0");
+
+  print('Informe a operação matemática (+, -, *, /): ');
+  line = stdin.readLineSync(encoding: utf8);
+  var operacao = line ?? "0";
 }

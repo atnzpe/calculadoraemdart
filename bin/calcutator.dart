a66df20ea9df528126608def60ac86c86e8d91ed
Gleyson Atanázio <gleysonasilva@gmail.com>
2023-09-04 19:59:57 -0300

    Atualiza arquivos

---
+++ bin/calcutator.dart
@@ -0,0 +1,9 @@
+import 'package:calcutator/calcutator.dart' as calcutator;
+import 'dart:convert';
+import 'dart:io';
+
+void main(List<String> arguments) {
+  print('Bem vindo!');
+  print('Informe o primeiro número: ');
+  
+}
